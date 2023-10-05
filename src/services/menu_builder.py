39b144936564849b7f7e0343b485ef396f2681ed
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-10-05 15:17:29 -0300

    Feat: Requisito 4

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,18 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        menu = [
+            {
+                'dish_name': dish.name,
+                'ingredients': dish.get_ingredients(),
+                'price': dish.price,
+                'restrictions': dish.get_restrictions()
+            }
+            for dish in self.menu_data.dishes
+            if restriction not in dish.get_restrictions() and all(
+                ingredient in self.inventory.inventory
+                for ingredient in dish.get_ingredients()
+            )
+        ]
+
+        return menu
