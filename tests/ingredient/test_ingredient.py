b886f5d135e49f3cbf52787ca15e1ad48cb36571
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-10-05 13:39:31 -0300

    Feat: Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,12 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    instance = Ingredient('milk')
+    instance2 = Ingredient('chocolate')
+
+    assert instance.name == 'milk'
+    assert hash(instance) == hash('milk')
+    assert repr(instance) == "Ingredient('milk')"
+    assert instance == instance
+    assert instance != instance2
+    assert instance.restrictions == set()
